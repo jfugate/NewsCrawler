23ef9864cd0d24dd083bb564aef67943232aa32d
Jeremy Fugate <jeremycfugate@gmail.com>
2017-08-05 12:21:13 -0400

    remove api key. regenerated a new one
    went back to newsapi to regenerate a new key

--- new_york_times.py
+++ new_york_times.py
@@ -2,7 +2,7 @@ import urllib3, requests, json, BeautifulSoup, os
 
 def new_york_times():
 	news_source="the-new-york-times"
-	http_connect = requests.get("https://newsapi.org/v1/articles?source=" + news_source + "&apiKey=dbafac003ee64bd187bc760b5ba0dcb2") #+ os.envrion['API_KEY']).read()
+	http_connect = requests.get("https://newsapi.org/v1/articles?source=" + news_source + "&apiKey=") #+ os.envrion['API_KEY']).read()
 	json_response = json.loads(http_connect.text)
 	for i in range(0, len(json_response['articles'])):
 		title = json_response['articles'][i]['title']
